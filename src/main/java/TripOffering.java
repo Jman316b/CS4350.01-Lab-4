73eead402eb1c5e2ec161c6040d2e8b6cdca00eb
Joshua Jenkins <dajoesh@gmail.com>
2024-04-26 10:51:52 -0700

    started making every object needed for each table

--- src/main/java/TripOffering.java
+++ src/main/java/TripOffering.java
@@ -1,12 +1,15 @@
-public class TripOffering {
+import java.sql.*;
+
+public class TripOffering{
 	private int tripNumber;
-	private String date;
+	private Date date;
 	private String scheduledStartTime;
 	private String secheduledArrivalTime;
 	private String driverName;
 	private int busID;
 	
-	public TripOffering(int tripNumber, String date, String scheduledStartTime, String secheduledArrivalTime,
+	
+	public TripOffering(int tripNumber, Date date, String scheduledStartTime, String secheduledArrivalTime,
 		String driverName, int busID) {
 		this.tripNumber = tripNumber;
 		this.date = date;
@@ -25,11 +28,11 @@ public class TripOffering {
 	}
 
 	public String getDate() {
-		return date;
+		return date.toString();
 	}
 
-	public void setDate(String date) {
-		this.date = date;
+	public void setDate(String dateString) {
+		this.date = java.sql.Date.valueOf(dateString);
 	}
 
 	public String getScheduledStartTime() {
